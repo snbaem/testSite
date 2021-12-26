24b7436c7ecfc00c68c5664da805b7813e28cd64
snbaem <shubham.borole@gmail.com>
2021-12-26 15:12:07 -0500

    Initial Project check in

---
+++ ui.apps/src/main/content/jcr_root/apps/testsite/clientlibs/clientlib-react/resources/precache-manifest.ff6b5da9c6775b1543c663e59dffba4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8330e665a5bcb8325edcba66f5b1739f",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/index.html"
+  },
+  {
+    "revision": "33b2fded6a9f0448e9c5",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/static/css/main.2a17b812.chunk.css"
+  },
+  {
+    "revision": "23891aa255082f5f3431",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/static/js/2.3d98272e.chunk.js"
+  },
+  {
+    "revision": "049012c4a67f621c782fa4d39fa8969e",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/static/js/2.3d98272e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b2fded6a9f0448e9c5",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/static/js/main.7790db7d.chunk.js"
+  },
+  {
+    "revision": "26c717cfb1a1ffce12ef",
+    "url": "/etc.clientlibs/testsite/clientlibs/clientlib-react/resources/static/js/runtime-main.d702dabc.js"
+  }
+]);
